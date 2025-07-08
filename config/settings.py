c4d3f0430f4cd24cf1a53990be68592b2aee10f2
Haze@me <108361840+Haze-me@users.noreply.github.com>
2025-07-08 17:25:21 +0000

    updated

--- config/settings.py
+++ config/settings.py
@@ -17,9 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-qefk__3x*%ki)&*kdynp@koc%4kha^%bw514!mm9jrdb5f*s9#'
-
-#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 #DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
